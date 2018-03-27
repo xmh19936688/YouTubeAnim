75f1f0037a6ebdee2954f4bf479e7468295dfa2a
void <xmh19936688@163.com>
2018-03-27 12:49:07 +0800

    add five fragments

--- app/src/main/java/com/xmh/youtubeanim/MainActivity.java
+++ app/src/main/java/com/xmh/youtubeanim/MainActivity.java
@@ -1,13 +1,80 @@
 package com.xmh.youtubeanim;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.xmh.youtubeanim.fragment.ActivityFragment;
+import com.xmh.youtubeanim.fragment.HomeFragment;
+import com.xmh.youtubeanim.fragment.MediaFragment;
+import com.xmh.youtubeanim.fragment.PopularFragment;
+import com.xmh.youtubeanim.fragment.SubscribeFragment;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewPager vpFragments;
+
+    private HomeFragment mHomeFragment = new HomeFragment();
+    private PopularFragment mPopularFragment = new PopularFragment();
+    private SubscribeFragment mSubscribeFragment = new SubscribeFragment();
+    private ActivityFragment mActivityFragment = new ActivityFragment();
+    private MediaFragment mMediaFragment = new MediaFragment();
+    private List<Fragment> mFragments = Arrays.asList(mHomeFragment, mPopularFragment, mSubscribeFragment, mActivityFragment, mMediaFragment);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initView();
+    }
+
+    private void initView() {
+        vpFragments = findViewById(R.id.vp_fragments);
+        vpFragments.setAdapter(new FragmentAdapter(getSupportFragmentManager()));
+    }
+
+    public void onNavigationClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_home:
+                vpFragments.setCurrentItem(mFragments.indexOf(mHomeFragment));
+                break;
+            case R.id.btn_activity:
+                vpFragments.setCurrentItem(mFragments.indexOf(mActivityFragment));
+                break;
+            case R.id.btn_media:
+                vpFragments.setCurrentItem(mFragments.indexOf(mMediaFragment));
+                break;
+            case R.id.btn_popular:
+                vpFragments.setCurrentItem(mFragments.indexOf(mPopularFragment));
+                break;
+            case R.id.btn_subscribe:
+                vpFragments.setCurrentItem(mFragments.indexOf(mSubscribeFragment));
+                break;
+        }
+    }
+
+    private class FragmentAdapter extends FragmentPagerAdapter {
+
+        public FragmentAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragments.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragments.size();
+        }
     }
 }
---
+++ app/src/main/java/com/xmh/youtubeanim/fragment/PopularFragment.java
@@ -0,0 +1,26 @@
+package com.xmh.youtubeanim.fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.xmh.youtubeanim.R;
+
+/**
+ * Created by void on 2018.03.27 027.
+ */
+
+public class PopularFragment extends Fragment {
+
+    private View mRootView;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mRootView = inflater.inflate(R.layout.fragment_popular, container, false);
+        return mRootView;
+    }
+}
